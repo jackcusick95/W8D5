91f99b6f92102c7f811f1c8e508c14a7b88b48ea
syldysnya <syldysnya@gmail.com>
2021-04-16 13:51:33 -0400

    Set #constructor

--- project.js
+++ project.js
@@ -1,13 +1,15 @@
 class Clock {
     constructor() {
-        this.date = new Date();
-        this.time = time;
-        clock.printTime.call();
+        const newDate = new Date();
+        this.hours = newDate.getHours();
+        this.mins = newDate.getMinutes();
+        this.secs = newDate.getSeconds();
+
+        this.printTime.call();
         this.setInterval(function () {
-            console.log("tick")
+            this._tick();
         }, 1000);
 
-        }
         // 1. Create a Date object.
         // 2. Store the hours, minutes, and seconds.
         // 3. Call printTime.
@@ -15,14 +17,17 @@ class Clock {
     }
 
     printTime() {
+        console.log(`${this.hours}:${this.mins}:${this.secs}`)
         // Format the time in HH:MM:SS
         // Use console.log to print it.
     }
 
     _tick() {
+        
         // 1. Increment the time by one second.
         // 2. Call printTime.
     }
 }
 
 const clock = new Clock();
+// clock.printTime();
